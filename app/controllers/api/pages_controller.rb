2ddbee6218482316fc08c5d32049b67189d544a8
Christian Green <hoolio.suare@gmail.com>
2022-08-29 19:43:07 -0600

    updates

---
+++ app/controllers/api/pages_controller.rb
@@ -0,0 +1,39 @@
+class Api::PagesController < ApplicationController
+  def index
+    # @pages = Page.all
+    # render json: @pages
+    render json: Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+    render json: @page
+  end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      render json: @page
+    else
+      render json: {errors: @page.errors }, status: : unprocessable_entity
+
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(page_params)
+      render json: {errors: @page.errors }, status: : unprocessable_entity
+    end
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    render json: { message: 'Page deleted'}
+  end
+
+  private 
+  #only have access to in this class.
+  def page_params
+    params.require(:page).permit(:title, :author, :body)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # # root "articles#index"
+  # get '/pages', to: 'pages#index'
+  # post '/pages', to: 'pages#create'
+  namespace :api do
+  
+  end
 end
