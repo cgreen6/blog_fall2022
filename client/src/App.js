ee9141a35f65aee4bc322e1a5bcca2b51b7ae999
Christian Green <hoolio.suare@gmail.com>
2022-08-29 20:47:58 -0600

    end of lecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 
 import Pages from './components/pages/Pages';
 import PageForm from './components/pages/PageForm';
+import Navbar from './components/shared/Navbar';
+import Home from './components/shared/Home';
+import About from './components/shared/About';
+
 
 const App = () => (
   <>
---
+++ client/src/components/shared/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+      <a href="/">About</a>
+      </li>
+        
+      <li>
+      <a href="/">Navbar</a>
+      </li>
+    </ul>
+
+
+  </nav>
